52594c25313c646a29733f205e6ddb328a519e78
Palaash Atri <40315170+palaashatri@users.noreply.github.com>
2024-10-01 14:00:33 +0530

    Update install_zing.sh

--- scripts/bookworm/install_zing.sh
+++ scripts/bookworm/install_zing.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-set -ex
 
-JDK_VERSION=21.0.0
+echo "============================================================================================================================"
+echo "Quick setup script to install Azul Zing JDK 21 (Stream Build) on Debian Bookworm machine"
+echo "Based on instructions published at : https://docs.azul.com/prime/prime-quick-start-apt "
+echo "Stream Builds are produced once a month, do not receive any security backports, and are free for development and evaluation."
+echo "============================================================================================================================"
 
 curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
 echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zing/debian bookworm main" | sudo tee /etc/apt/sources.list.d/zing.list
+
 sudo apt update
-sudo apt install zing-jdk$JDK_VERSION -y
+sudo apt install zing-jdk21.0.0 -y
+
+echo "============================================================================================================================"
+echo "May need to set JAVA_HOME. In case its not set, use : export JAVA_HOME=/opt/zing/zing-<jdk-version>"
+echo "============================================================================================================================"
+echo "Printing \"java -version\" ... "
+java -version
+echo "============================================================================================================================"
