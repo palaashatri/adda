f01b69902254172ed0d127128b7d51f57b04ffc0
Palaash Atri <40315170+palaashatri@users.noreply.github.com>
2024-12-03 10:05:21 +0530

    Create fix-yum.sh

---
+++ scripts/centos/fix-yum.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script fixes yum install on centos 7 machines so that we can install new packages.
+# changes remote server for yum to pull updates from
+# packages are definitely not on the latest version, but atleast its something.
+set -ex
+PACKAGES="gdb"
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*.repo
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*.repo
+yum clean all; yum makecache
+yum update && yum install $PACKAGES -y
