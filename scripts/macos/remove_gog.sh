8df75b72a47bff924fb02600258512d9ee2f2d65
Palaash Atri <40315170+palaashatri@users.noreply.github.com>
2025-11-08 16:32:27 +0530

    Update success message in remove_gog.sh

--- scripts/macos/remove_gog.sh
+++ scripts/macos/remove_gog.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-set +ex
 find / -not \( -path '/System/Volumes/*' -prune \) \( -iname '*com.gog.*' -o -iname '*gog.com*' \) -prune 2>/dev/null
-echo "Success!"
+echo "Successfully removed GOG Galaxy!"
