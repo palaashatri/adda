49c142be1374b0e5212ebdf9512010318125066f
Palaash Atri <patri@azul.com>
2026-04-09 17:31:34 +0530

    dd-practice

---
+++ practice/rust/hello_with_input.rs
@@ -0,0 +1,13 @@
+fn main() 
+{
+    use std::io;
+
+    println!("Please enter your name: ");
+
+    let mut name = String::new();
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    println!("Hello, {}!", name.trim());
+}
